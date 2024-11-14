ed5afa448b234c89ce95093d06e1e8d8192ed2c4
Raduuuk <112314477+Raduuuk@users.noreply.github.com>
2024-11-14 09:35:22 +0200

    Update Measurements_Send_Final.ino
    RTC add

--- Measurements_Send_Final.ino
+++ Measurements_Send_Final.ino
@@ -1,21 +1,35 @@
 #include <DFRobot_SIM7000.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <RTClib.h>
 
-#define PIN_TX     7
-#define PIN_RX     8
+#define PIN_TX 7
+#define PIN_RX 10
 #define THINGSPEAK_API_KEY "TBU138X3L580GACK"
 
 #define VOLTAGE_PIN A0
 #define CURRENT_PIN A1
 
-SoftwareSerial     mySerial(PIN_RX, PIN_TX);
-DFRobot_SIM7000    sim7000(&mySerial);
+SoftwareSerial mySerial(PIN_RX, PIN_TX);
+DFRobot_SIM7000 sim7000(&mySerial);
 
-struct Reading {
+RTC_DS3231 rtc; //rtc module
+
+int count = 250;
+
+struct Reading { // define datatime,milliseconds,V,C
+  DateTime dateTime;
+  unsigned long milliseconds;
   float voltage;
   float current;
-  
 };
+
+Reading readings[250]; // Array to store 40 readings
+volatile int readingCount = 0; // Counter for the number of readings taken
+
+void initSIM7000();
+Reading takeReading();
+void printReading(Reading reading);
 
 void setup() {
   Serial.begin(9600);
@@ -24,58 +38,79 @@ void setup() {
   // SIM7000 setup
   initSIM7000();
 
-  // Add a delay before starting to take readings
-  delay(1000);
+  if (!rtc.begin()) { //define rtc midule
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) { // check Power
+    Serial.println("RTC lost power, let's set the time!");
+    rtc.adjust(DateTime(F(DATE), F(TIME)));
+  }
+
 }
 
 void loop() {
-  Reading reading = takeReading();
-  printReading(reading);
-  sendDataToThingspeak(reading);
-  delay(15000); // 15 seconds delay
+  unsigned long previousMicros = 0; // will store the last time a reading was taken
+
+  for (int i = 0; i < count; i++) {
+    unsigned long currentMicros = micros();
+
+    if (currentMicros - previousMicros >= 4000) { // at least 4ms passed since last reading
+      Reading newReading = takeReading();
+      readings[readingCount] = newReading;
+      readingCount++;
+
+      previousMicros = currentMicros; // save the time a reading was taken
+    }
+
+    while (micros() - previousMicros < 4000); // wait for the remainder of the 4ms period
+  }
+
+  // Process and print the readings
+  for (int i = 0; i < count; i++) {
+    printReading(readings[i]);
+  }
+
+  readingCount = 0; // Reset readingCount
+
+  delay(5000); // delay between sets of readings
 }
 
+
 void initSIM7000() {
-  // SIM7000 initialization and setup here (as in the first code)
+  // SIM7000 initialization and setup here
+  // Initialize the SIM7000 module, set APN, etc.
 }
 
 Reading takeReading() {
-  float voltage = analogRead(VOLTAGE_PIN) * 5.0 / 1023.0;
-  float current = analogRead(CURRENT_PIN) * 5.0 / 1023.0;
+  DateTime now = rtc.now(); //set up a time for RTC module
+  
+  unsigned long milliseconds = millis();
+  float voltage = analogRead(VOLTAGE_PIN) * (5.0 / 1023.0);
+  float current = analogRead(CURRENT_PIN) * (5.0 / 1023.0);
 
-  return {voltage, current};
+  return {now, milliseconds, voltage, current};
 }
 
 void printReading(Reading reading) {
-  Serial.print(" Voltage: ");
-  Serial.print(reading.voltage);
-  Serial.print("V, ");
-  Serial.print("Current: ");
-  Serial.print(reading.current);
-  Serial.println("A");
-}
-
-void sendDataToThingspeak(Reading reading) {
-  if (sim7000.openNetwork(DFRobot_SIM7000::eTCP, "api.thingspeak.com", 80) != 0) {
-    Serial.println("Error attaching to network");
-    return;
-  }
-
-  String url = "GET /update?api_key=" + String(THINGSPEAK_API_KEY) + "&field3=" + String(reading.voltage) + "&field4=" + String(reading.current) + " HTTP/1.1\r\nHost: api.thingspeak.com\r\nConnection: close\r\n\r\n";
-
-  Serial.print("Sending request to ThingSpeak: ");
-  Serial.println(url);
-
-  sim7000.send(url.c_str());
-
-  // Wait for the response
-  uint32_t timeout = millis();
-  while (!sim7000.recv("CLOSED", 1000)) {
-    if (millis() - timeout > 30000) {
-      Serial.println(" No response received within timeout");
-      break;
-    }
-  }
-
-  sim7000.closeNetwork();
+  Serial.print("Voltage: ");
+  Serial.print(reading.voltage, 2);
+  Serial.print("V, Current: ");
+  Serial.print(reading.current, 2);
+  Serial.print("A, Timestamp: ");
+  Serial.print(reading.dateTime.year(), DEC);
+  Serial.print('/');
+  Serial.print(reading.dateTime.month(), DEC);
+  Serial.print('/');
+  Serial.print(reading.dateTime.day(), DEC);
+  Serial.print(" ");
+  Serial.print(reading.dateTime.hour(), DEC);
+  Serial.print(':');
+  Serial.print(reading.dateTime.minute(), DEC);
+  Serial.print(':');
+  Serial.print(reading.dateTime.second(), DEC);
+  Serial.print('.');
+  Serial.println(reading.milliseconds, DEC);
+  
 }
